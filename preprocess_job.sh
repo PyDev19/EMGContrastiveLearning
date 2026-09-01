c2e76fea8444b3423ff912ceed7505163b8a1e9c
Atharva Mishra <atharvamishra956+1@gmail.com>
2026-08-31 22:47:00 -0500

    added SLURM job for preprocessing

---
+++ preprocess_job.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --partition=normal
+#SBATCH --mail-user=axm240143@utdallas.edu
+#SBATCH --mail-type=ALL
+#SBATCH --job-name=project-blueprint-preprocess
+#SBATCH --output=preprocess_output.txt
+
+module load miniconda
+
+conda init bash
+source activate base
+conda activate meyerlab
+
+SRC=/groups/emeyers/ProjectBlueprint/
+
+cd ~/scratch/
+
+hf download formove-ai/physiomio --repo-type dataset --local-dir ./blueprint_data/
+
+python $SRC/src/preprocessing/physiomio.py --data_dir ./blueprint_data/data/ --output_dir ./blueprint_data/impaired_preprocessed/ --include_fma_zero
